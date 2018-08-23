26461e747ba8c0f4f43a990915dad3aba847abb9
Josie00 <xl4tn@virginia.edu>
2018-08-23 12:19:20 -0400

    init commit

---
+++ GitDashBoard/src/main/java/com/github/com/GitDashBoardApplication.java
@@ -0,0 +1,12 @@
+package com.github.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDashBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDashBoardApplication.class, args);
+	}
+}
