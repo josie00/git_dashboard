eaef69c23e659e8605929829a926a347a2ede1da
Josie00 <xl4tn@virginia.edu>
2018-09-02 15:01:45 -0700

    deploy

---
+++ deploy/download.py
@@ -0,0 +1,19 @@
+import os
+from datetime import datetime, timedelta
+from subprocess import call
+if __name__ == "__main__":
+    now = datetime.today()
+    os.system("mkdir test_data")
+    for x in range(0, 7):
+        now = now - timedelta(days=1)
+        month = str(now.month)
+        day = str(now.day)
+        if(len(month) == 1):
+            month = "0" + month
+        if (len(day) == 1):
+            day = "0" + day
+        date = str(now.year) + "-" + month + "-" + day;
+        os.system("wget http://data.gharchive.org/" + date +"-{0..23}.json.gz -P ./test_data")
+        os.system("gunzip ./test_data/" + date+"-{0..23}.json.gz")
+
+
