2e47926d3450f7aacccfb2346f0777fb7357ced0
Josie00 <xl4tn@virginia.edu>
2018-09-02 12:30:47 -0700

    update download py

--- git_dashboard/download.py
+++ git_dashboard/download.py
@@ -1,8 +1,18 @@
-
+import os
 from datetime import datetime, timedelta
 from subprocess import call
 if __name__ == "__main__":
-    now = datetime.today() - timedelta(days=1)
-    date = str(now.year) + "-" + str(now.month) + "-" + str(now.day);
-    for x in range(0, 1):
-        os.system("wget http://data.gharchive.org/" + date +"-{0..23}.json.gz -O ./test_data")
+    now = datetime.today()
+    for x in range(0, 2):
+        now = now - timedelta(days=1)
+        month = str(now.month)
+        day = str(now.day)
+        if(len(month) == 1):
+            month = "0" + month
+        if (len(day) == 1):
+            day = "0" + day
+        date = str(now.year) + "-" + month + "-" + day;
+        os.system("wget http://data.gharchive.org/" + date +"-{0..23}.json.gz -P ./test_data")
+        os.system("gunzip ./test_data/" + date+"-{0..23}.json.gz")
+
+
